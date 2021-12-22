bb6f451d4e895e7a66157049a5212499136028c0
rmfranca <rmdfra@gmail.com>
2021-12-22 15:49:03 -0300

    ggplot now takes the `breaks` argument, which adjusts the y axis marks according to the asset returns.

--- myriskparity.R
+++ myriskparity.R
@@ -164,7 +164,7 @@ ggplot(tidy_rt, aes(x = date, y = all_rt, color = assets)) +
   labs(y="Returns", 
        x = "Date", 
        title = "Risk parity - cumulative returns since 2Q2011") +
-  scale_y_continuous(labels = percent_format(accuracy = 0.01)) + 
+  scale_y_continuous(labels = percent_format(accuracy = 0.01),breaks = seq(0, max(tidy_rt$all_rt), by = 0.5)) + 
   scale_color_manual(name = "Assets",
                      values = c("seagreen",
                                 "cyan",
